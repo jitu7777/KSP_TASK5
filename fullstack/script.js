8bb8408de05bb860a0d82f8a0000d26d50df2925
jitu7777 <150154530+jitu7777@users.noreply.github.com>
2023-11-13 12:50:29 +0530

    Add files via upload

---
+++ fullstack/script.js
@@ -0,0 +1,43 @@
+window.alert("this is jitu");
+
+console.log("Hello");
+
+//Variable assign
+number1 = 4;
+
+console.log(number1);
+
+//let assign
+let number = 20;
+
+console.log(number);
+
+//CamelCase
+thisIsTaskNumber = "task number 5";
+
+console.log(thisIsTaskNumber);
+
+//Constant
+const name = "jithendra";
+
+console.log(name);
+
+/*
+
+
+this 
+is 
+multi-line
+comment
+
+*/
+
+// Operators
+let num1 = 2;
+let num2 = 4;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
